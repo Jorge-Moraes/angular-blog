67e6290a63d72e7f9dbcd3a7b41874186b9bc247
Jorge-Moraes <jorge-ricardo15@hotmail.com>
2023-03-31 11:40:05 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Monkey D. Luffy",
+        "description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. ",
+        "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2021/05/luffy-e-o-personagem-favorito-dos-leitores-de-one-piece.jpg"
+    },
+    {
+        "id": "2",
+        "title": "Zoro",
+        "description": " ",
+        "photoCover": "https://johto.legiaodosherois.com.br/wp-content/uploads/2021/11/legiao_XHYJRSWAV1xl.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Usopp",
+        "description": " ",
+        "photoCover": "https://i.pinimg.com/originals/ac/3b/90/ac3b90ffd6cccdba348ba518929083df.jpg"
+    },
+    {
+        "id": "4",
+        "title": "Sanji",
+        "description": " ",
+        "photoCover": "https://images2.alphacoders.com/124/1240710.png"
+    },
+];
